aa3735eda407bc0eded3cd4acc38deb3d856f075
NotTacosHub <smithclayton80@gmail.com>
2021-12-02 19:54:38 -0600

    updated end points

--- front/app.py
+++ front/app.py
@@ -3,32 +3,32 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/")
+@app.route("/")
 def index():
     return render_template('index.html')
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/blog")
+@app.route("/blog")
 def blog():
     return render_template('post_list.html')
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/blog/post/<slug>")
+@app.route("/blog/post/<slug>")
 def blog_post(slug):
     return render_template('post.html', slug=slug)
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/admin/post/create")
+@app.route("/admin/post/create")
 def admin_post_create():
     return render_template('admin_post_create.html')
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/admin/post/update/<slug>")
+@app.route("/admin/post/update/<slug>")
 def admin_post_update(slug):
     return render_template('admin_post_update.html', slug=slug)
 
 
-@app.route("/https://clayton-smith-capstone-project.herokuapp.com/admin/post/list")
+@app.route("/admin/post/list")
 def admin_post_list():
     return render_template('admin_post_list.html')
 
